e845fcb682c6d74c096039a89248958604e5a5bf
Christopher Bell <christopherbell@outlook.com>
2022-02-24 16:35:06 +0000

    Add project files.

---
+++ DapperCodeGenerator/Data/Tablefield.cs
@@ -0,0 +1,28 @@
+﻿namespace DapperCodeGenerator.Data
+{
+    public class Tablefield
+    {
+        // Auto-implemented properties.
+        public string Fieldname { get; set; } = null!;
+        public string SQLtype { get; set; } = null!;
+        public string Codetype { get; set; } = null!;
+        public string DBtype { get; set; } = null!;
+        public int Length { get; set; }
+        public bool IsRequired { get; set; }
+
+        public Tablefield()
+        {
+
+        }
+
+        public Tablefield(string fieldname, string sqlytpe, string codetype, string dbtype, int length, bool isrequired)
+        {
+            this.Fieldname = fieldname;
+            this.SQLtype = sqlytpe;
+            this.Codetype = codetype;
+            this.DBtype = dbtype;
+            this.Length = length;
+            this.IsRequired = isrequired;
+        }
+    }
+}
